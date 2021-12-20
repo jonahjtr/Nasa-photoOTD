869b50ccb82851f5f2f10f4d16fb6b0e09c68af2
jonah.jtr <tillmanjonah@gmail.com>
2021-12-20 04:05:00 -0600

    formatting code and adding gitignore

--- src/components/Explanation.js
+++ src/components/Explanation.js
@@ -1,47 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledDiv = styled.div`
-  background-color: ${pr => pr.theme.primaryColor};
+  background-color: ${(pr) => pr.theme.primaryColor};
   color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
 
-h2{
+  h2 {
     padding-top: 4%;
     font-size: 2rem;
-}
-h3{
+  }
+  h3 {
     font-size: 2rem;
-   
-}
-p{
+  }
+  p {
     display: flex;
     font-size: 1.5rem;
-    margin: 0 10%;  
-    padding:  2%;
+    margin: 0 10%;
+    padding: 2%;
     width: 80%;
-
-}
-`
+  }
+`;
 const StyleDiv2 = styled.div`
-border: black 1px solid;
-width: 60%;
-margin: 0 10%;
-`
+  border: black 1px solid;
+  width: 60%;
+  margin: 0 10%;
+`;
 
-function Explanation(props){
-    return(
-        <StyledDiv>
-            
-            <h3>{props.date}</h3>
-            <StyleDiv2>
-            </StyleDiv2>
-            <p>{props.explanation}</p>
-            <h2>{props.copyright}</h2>  
-            <h6>{props.version}</h6>
-        </StyledDiv>
-    )
+function Explanation(props) {
+  return (
+    <StyledDiv>
+      <h3>{props.date}</h3>
+      <StyleDiv2></StyleDiv2>
+      <p>{props.explanation}</p>
+      <h2>{props.copyright}</h2>
+      <h6>{props.version}</h6>
+    </StyledDiv>
+  );
 }
-export default Explanation
+export default Explanation;
